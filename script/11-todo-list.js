3de5ace2f95443f3f405681c8a6c4a00e4439c9e
chengenkuo <123361082+chengenkuo@users.noreply.github.com>
2023-04-25 15:32:03 +0800

    Add files via upload

---
+++ script/11-todo-list.js
@@ -0,0 +1,50 @@
+const todoList = [{
+  name:'make dinner',
+  dueDate:'2023-04-09'
+}, {
+  name:'wash dishes',
+  dueDate:'2023-04-09'
+}];
+   
+
+renderTodoList();
+function renderTodoList(){
+  let todoListHTML = '';
+  for(let i = 0; i < todoList.length; i++){
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+  `;
+    todoListHTML += html;
+  }
+  
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector('.due-date-input');
+  const dueDate = dateInputElement.value;
+  console.log(name);
+
+  todoList.push({
+    name,
+    dueDate
+  });
+  
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
+
